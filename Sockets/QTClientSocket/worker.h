dfc498157f3e809f86e03c11cbc5b99456bd9fc3
paulnta <paul.ntawuruhunga@heig-vd.ch>
2015-12-28 00:09:15 +0100

    sleep 1 sec

--- Sockets/QTClientSocket/worker.h
+++ Sockets/QTClientSocket/worker.h
@@ -28,7 +28,7 @@ public slots:
 
         for(int i=0; i < 10; i++){
             sendData(QString("data %1 \n").arg(i).toUtf8());
-            sleep(5);
+            sleep(1);
         }
 
         sendData("STOP");
